504ddab034b9e29e90f81c0c57cf54d5d7209367
Jonathan Okz <jonathanolesz@gmail.com>
2025-08-11 22:23:57 +0200

    timestamp ms patch 6

--- crates/ethereum/payload/src/lib.rs
+++ crates/ethereum/payload/src/lib.rs
@@ -160,7 +160,7 @@ where
             &mut db,
             &parent_header,
             NextBlockEnvAttributes {
-                timestamp: reth_primitives::time::normalize_timestamp_to_seconds(attributes.timestamp()),
+                timestamp: attributes.timestamp(),
                 suggested_fee_recipient: attributes.suggested_fee_recipient(),
                 prev_randao: attributes.prev_randao(),
                 gas_limit: builder_config.gas_limit(parent_header.gas_limit),
